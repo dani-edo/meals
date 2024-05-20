a4f80130460b56d49f61b997105adc191a283b4d
dani_edo <dani.edogawa.17@gmail.com>
2024-05-20 21:03:03 +0700

    setup google fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,6 +15,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        textTheme: GoogleFonts.latoTextTheme(),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
