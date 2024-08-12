c01cbf1d7d3000066e652fe60b737464450391fd
dani_edo <dani.edogawa.17@gmail.com>
2024-08-12 18:55:11 +0700

    meals item with transparent_image on the first load

--- lib/screens/meals.dart
+++ lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({
@@ -16,11 +17,8 @@ class MealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     Widget content = ListView.builder(
       itemCount: meals.length, // harus diisi jika menggunakan ListView
-      itemBuilder: (ctx, index) => Text(
-        meals[index].title,
-        style: const TextStyle(
-          color: Colors.white,
-        ),
+      itemBuilder: (ctx, index) => MealItem(
+        meal: meals[index],
       ),
     );
 
