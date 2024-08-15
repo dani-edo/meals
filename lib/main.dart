d56b4fa8210b94f5d8827ba106854e2d62c3a3e3
dani_edo <dani.edogawa.17@gmail.com>
2024-08-15 14:29:35 +0700

    tabs screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:meals/screens/categories.dart';
+import 'package:meals/screens/tabs.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
         textTheme: GoogleFonts.latoTextTheme(),
       ),
-      home: const CategoriesScreen(),
+      home: const TabsScreen(),
     );
   }
 }
