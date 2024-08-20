d6fe7fdbb7bab3f77de34f8e1c5db1837281834c
dani_edo <dani.edogawa.17@gmail.com>
2024-08-20 22:34:17 +0700

    feat: flutter_riverpod: install, setup provider, and state consume

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals/screens/tabs.dart';
+import 'package:riverpod/riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
